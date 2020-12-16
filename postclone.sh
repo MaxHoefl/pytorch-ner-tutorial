975ff17199218815ab2e4bdea0238fb29a84e7f3
Ubuntu <ubuntu@ip-172-31-16-19.eu-central-1.compute.internal>
2020-12-16 09:14:21 +0000

    postclone steps

---
+++ postclone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#pip install pipenv
+#pipenv install
+
+mkdir -p data/
+echo "Enter kaggle API credentials: "
+read -p "username: " username
+read -p "password: " password
+export KAGGLE_USERNAME=$username
+export KAGGLE_KEY=$password
+pipenv run kaggle datasets download abhinavwalia95/entity-annotated-corpus
+unzip entity-annotated-corpus -d data/
+rm entity-annotated-corpus.zip
+pipenv run python src/build_kaggle_dataset.py
+pipenv run python src/build_vocab.py --data_dir data/
+
+
