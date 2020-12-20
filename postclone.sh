4e81178c29fd309e9c986eebf8b1a2af4a514a65
Ubuntu <ubuntu@ip-172-31-16-19.eu-central-1.compute.internal>
2020-12-20 20:29:53 +0000

    postclone

--- postclone.sh
+++ postclone.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
+repodir=$(pwd)
+
 ## Vim installation
-mkdir -p ~/tmp/
-cd ~/tmp/
-git clone https://github.com/MaxHoefl/vimrc.git
-cd vimrc
-sudo chmod +x install.sh
-./install.sh
+function install_vim {
+    mkdir -p ~/tmp/
+    cd ~/tmp/
+    if [ -d vimrc/ ]; then
+        sudo cp -r vimrc vimrc_bkup
+        rm -rf vimrc
+        git clone https://github.com/MaxHoefl/vimrc.git
+        cd vimrc/
+    fi
+    ./install.sh
+    cd $repodir
+}
 
 ## Python dependency installation
-pip install pipenv
-pipenv install
+function setup_pipenv {
+    pip install pipenv
+    pipenv install
+}
 
 ## Downloading data required for NLP model
-mkdir -p data/
-echo "Enter kaggle API credentials: "
-read -p "username: " username
-read -p "password: " password
-export KAGGLE_USERNAME=$username
-export KAGGLE_KEY=$password
-pipenv run kaggle datasets download abhinavwalia95/entity-annotated-corpus
-unzip entity-annotated-corpus -d data/
-rm entity-annotated-corpus.zip
+function download_dataset {
+    mkdir -p data/
+    echo "Enter kaggle API credentials: "
+    read -p "username: " username
+    read -p "password: " password
+    export KAGGLE_USERNAME=$username
+    export KAGGLE_KEY=$password
+    pipenv run kaggle datasets download abhinavwalia95/entity-annotated-corpus
+    unzip entity-annotated-corpus -d data/
+    rm entity-annotated-corpus.zip
+}
 
 ## Preprocessing data
-pipenv run python src/build_kaggle_dataset.py
-pipenv run python src/build_vocab.py --data_dir data/
+function process_dataset {
+    pipenv run python src/build_kaggle_dataset.py
+    pipenv run python src/build_vocab.py --data_dir data/
+}
 
-
+install_vim
